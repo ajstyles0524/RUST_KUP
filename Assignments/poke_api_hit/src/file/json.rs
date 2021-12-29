47dcfea1674520f0c0a98db69377df2170df0d5e
Anand <71371404+ajstyles0524@users.noreply.github.com>
2021-12-29 14:14:38 +0530

    Update json.rs

--- Assignments/poke_api_hit/src/file/json.rs
+++ Assignments/poke_api_hit/src/file/json.rs
@@ -8,18 +8,16 @@ use std::collections::HashMap;
 ///
 /// #Return
 ///
-/// Return reqwest::Result<()> type
-pub fn hit_point() -> Result<(), reqwest::Error> {
-    env_logger::init();
+/// Return reqwest::Result<i64> type
+pub fn hit_point(key:String) -> reqwest::Result<i64> {
     let content = reqwest::blocking::get("https://pokeapi.co/api/v2/pokemon-species/ditto")?
         .json::<HashMap<String, serde_json::Value>>()?;
-    let resp_1 = &content["flavor_text_entries"][0]["flavor_text"];
-    let resp_2 = &content["color"]["name"];
-    let resp_3 = &content["flavor_text_entries"][3]["version"]["name"];
-    let resp_4 = &content["capture_rate"];
-    info!("{:#?}", resp_1);
-    info!("{:#?}", resp_2);
-    info!("{:#?}", resp_3);
-    info!("{:#?}", resp_4);
-    Ok(())
+    let resp_2 = content[&key].as_i64();
+    let value = match resp_2
+    {
+        Some(value) => value,
+        _=> 0,
+    };
+    info!("{:?}", (resp_2));
+    Ok(value)
 }
