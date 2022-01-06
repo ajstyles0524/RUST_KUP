918bdc05f2b0dc08074fd45d6df3fcd3869bd439
Anand <71371404+ajstyles0524@users.noreply.github.com>
2022-01-06 12:40:38 +0530

    Update matrix_multiplication.rs

--- Assignments/number_manipulation/src/operation/matrix_multiplication.rs
+++ Assignments/number_manipulation/src/operation/matrix_multiplication.rs
@@ -1,34 +1,36 @@
 use log::*;
-#[derive(Debug, PartialEq)]
-/// Matrix structure
-pub struct Matrix {
-    pub element: [[i32; 3]; 3],
-}
-impl Matrix {
-    /// matrix_multiplication method is used to calculate multiplication of two matrix
-    ///
-    /// #Arguments
-    ///
-    /// first_matrix:- first_matrix is of type Matrix
-    /// second_matrix:- second_matrix is of type Matrix
-    ///
-    /// #Return
-    ///
-    /// Return Matrix type
-    pub fn matrix_multiplication(first_matrix: Matrix, second_matrix: Matrix) -> Matrix {
-        info!("Calculating multiplication of two matrix");
-        let mut result = Matrix {
-            element: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
-        };
-        for index_1 in 0..3 {
-            for index_2 in 0..3 {
-                for index_3 in 0..3 {
-                    result.element[index_1][index_2] += first_matrix.element[index_1][index_3]
-                        * second_matrix.element[index_3][index_2];
-                }
-            }
-        }
-        info!("Multiplication of two matrix is returned");
-        result
+/// matrix_multiplication method is used to calculate multiplication of two matrix
+///
+/// #Arguments
+///
+/// first_matrix:- first_matrix is a reference of Vec<Vec<i32>>
+/// second_matrix:- second_matrix is a reference of Vec<Vec<i32>>
+///
+/// #Return
+///
+/// No Return
+pub fn matrix_multiplication(
+    first_matrix: &[Vec<i32>],
+    second_matrix: &[Vec<i32>],
+) -> Result<Vec<Vec<i32>>, String> {
+    if first_matrix[0].len() != second_matrix.len() {
+        return Err("Matrix Multiplication is not possible".to_string());
     }
+    let mut result: Vec<Vec<i32>> = Vec::new();
+    let l_1 = first_matrix.len();
+    let l_2 = second_matrix.len();
+    for (index_1, _item) in first_matrix.iter().enumerate().take(l_1)  {
+        println!("{}",index_1);
+        let mut vec: Vec<i32> = Vec::new();
+        for index_2 in 0..second_matrix[0].len() {
+            let mut sum = 0;
+            for (index_3, _index) in second_matrix.iter().enumerate().take(l_2){
+                sum += first_matrix[index_1][index_3] * second_matrix[index_3][index_2];
+            }
+            vec.push(sum);
+        }
+        result.push(vec);
+    }
+    info!("Multiplication of two matrix is returned successfully");
+    Ok(result)
 }
